28f464876b6268defbcb09c97066455d0e656854
SJong42 <100083016+SJong42@users.noreply.github.com>
2023-08-09 18:44:24 +0900

    Update models_kcat.py

--- models_kcat.py
+++ models_kcat.py
@@ -137,9 +137,9 @@ class MLPDecoder(nn.Module):
         self.fc4 = nn.Linear(out_dim, binary)
 
     def forward(self, x):
-        x = self.F.relu(self.fc1(x))
-        x = self.F.relu(self.fc2(x))
-        x = self.F.relu(self.fc3(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         x = self.fc4(x)
         return x
 
