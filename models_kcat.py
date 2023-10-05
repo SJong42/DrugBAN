f5bd7295befcee6268792d6388d6d2fc3e1604b4
SJong42 <100083016+SJong42@users.noreply.github.com>
2023-10-05 10:15:27 +0900

    Update models_kcat.py

--- models_kcat.py
+++ models_kcat.py
@@ -29,7 +29,7 @@ def cross_entropy_logits(linear_output, label, weights=None):
     max_class = class_output.max(1)
     y_hat = max_class[1]  # get the index of the max log-probability
     if weights is None:
-        loss = nn.NLLLoss()(class_output, label.type_as(y_hat).view(label.size(0)))
+        loss = nn.NLLLoss()(class_output.squeeze(), label.type_as(y_hat).view(label.size(0)))
     else:
         losses = nn.NLLLoss(reduction="none")(class_output, label.type_as(y_hat).view(label.size(0)))
         loss = torch.sum(weights * losses) / torch.sum(weights)
